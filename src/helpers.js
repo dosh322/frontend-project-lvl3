a47769b0e6cb8a8c3551ca8511d6170fc8f1e81a
Andrei Prokofev <palych322@gmail.com>
2021-01-29 14:00:06 +0300

    logging

--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
 import * as yup from 'yup';
 
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const proxyUrl = new URL('https://hexlet-allorigins.herokuapp.com/get?disableCache=true');
 
 export const makePosts = (feedId, items) => items
   .map((item) => ({ feedId, id: _.uniqueId(), ...item }));
 
 export const getRssLinks = (watched) => watched.feeds.map((feed) => feed.rssLink);
 
-export const addProxy = (url) => `${proxyUrl}${url}`;
+export const addProxy = (sourceUrl) => {
+  proxyUrl.searchParams.set('url', sourceUrl);
+  return proxyUrl;
+};
 
 export const validate = (rssLink, urls) => {
   const schema = yup.string().url('url')
